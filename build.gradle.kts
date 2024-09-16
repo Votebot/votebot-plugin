eb57e40943ca901e69adcd0aaa45ba4683c232f4
Michael Rittmeister <michael@rittmeister.in>
2024-09-16 02:37:53 +0200

    Fix sentry startup error

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
 }
 allprojects {
-    version = "5.5.1"
+    version = "5.5.2 "
     group = "space.votebot"
 
     repositories {

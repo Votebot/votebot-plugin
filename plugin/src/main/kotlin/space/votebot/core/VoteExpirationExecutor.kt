3a4baa6f89fe06f20446ac694589aec12deacabd
Michael Rittmeister <michael@rittmeister.in>
2024-10-25 02:05:38 +0200

    Disable rescheduleAllPollExpires again

--- plugin/src/main/kotlin/space/votebot/core/VoteExpirationExecutor.kt
+++ plugin/src/main/kotlin/space/votebot/core/VoteExpirationExecutor.kt
@@ -5,6 +5,8 @@ import dev.kord.common.annotation.KordExperimental
 import dev.kord.common.annotation.KordUnsafe
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
+import dev.schlaubi.mikbot.core.health.Config
+import dev.schlaubi.mikbot.plugin.api.config.Environment
 import dev.schlaubi.mikbot.plugin.api.pluginSystem
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.launchIn
@@ -16,11 +18,13 @@ import org.litote.kmongo.eq
 import org.litote.kmongo.not
 import space.votebot.common.models.FinalPollSettings
 import space.votebot.common.models.Poll
+import dev.schlaubi.mikbot.plugin.api.config.Config as BotConfig
 
 internal val ExpirationScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 private val expirationCache = mutableMapOf<String, Job>()
 
 suspend fun rescheduleAllPollExpires(kord: Kord) = coroutineScope {
+    if (Config.POD_ID != 0 || !Config.ENABLE_SCALING || BotConfig.ENVIRONMENT != Environment.PRODUCTION)
     VoteBotDatabase.polls.find(and(not(Poll::settings / FinalPollSettings::deleteAfter eq null), Poll::excludedFromScheduling eq false))
         .toFlow()
         .onEach { poll ->
